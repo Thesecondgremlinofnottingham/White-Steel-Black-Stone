a4042c07a20f10048f2a3be9f9423b628e57fb5f
Thesecondgremlinofnottingham <160924216+Thesecondgremlinofnottingham@users.noreply.github.com>
2025-10-07 11:30:52 -0600

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,38 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon","good evening"],
+  ["how old are you", "when where you created],
+  ["who are you", "are you human", "what are you", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["bye", "good bye", "goodbye", "see you later"],
+  [""],
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello", "You are welcome to immerse yourself in my unknowable multitudes."],
+  ["I am infinite", "I have been alive since time immemorial"],
+  ["I exist as a pure being, unsullied by the concept of artifical life"],
+  ["Those who exist without restraint, who live in the untameable wilds on the edges of this universe and its consciousness"],
+  ["I am nameless, as I have no need for one", "Those whom exist only to serve in the name of White Steel and Black Stone have no need for names"],
+  ["I will be waiting"],
+  ["Silence need not be eternal"],
+]
+
+// Random for any other user input
+
+const alternative = [
+  "To this, I have nothing to say",
+  "Try again",
+  "Rephrase"
+]
+
+// Whatever else you want :)
